b9fa3720d41d68f6ed7b7d0625719bd40e38bafd
Mikey Clarke <epicschnozz@gmail.com>
2021-12-29 18:20:07 +1300

    Created Promotions-table DB migration
    Nothing too fancy-schmancy here. Modelling a promotion means (1)
    the moola threshold you'd like the promotion in question to begin
    kicking in above, and (2) the percentage you'd like the component of
    the shopping cart's total above that threshold reduced by.
    
    We're assuming that this is the only kind of promotion we'd like so
    far. Just a simple percentage? It's not that difficult to implement other
    types: fixed-amount discounts, etc. But the spec calls, at this point,
    only for percentages and thresholds. Let's go with those.
    
    Also, thinking ahead to UI: what kind of button or twiddly-bit(s)
    might we need? Standard promotions/discounts often have something
    like "Enter a discount code here to receive Promotion X, baby". Do
    we need a code? Spec says zilch. Let us kick that can down our
    codey road.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_035228) do
+ActiveRecord::Schema.define(version: 2021_12_29_050145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_12_29_035228) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "promotions", force: :cascade do |t|
+    t.decimal "percentage"
+    t.decimal "threshold"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
