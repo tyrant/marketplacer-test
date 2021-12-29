1bd3fd594c76ffc023b510a51f62970fdb989540
Mikey Clarke <epicschnozz@gmail.com>
2021-12-29 18:44:21 +1300

    Populated seeds.db
    A few things going on here. The Seeds file is a finicky
    beastie, isn't it. The whole point is to populate the dev
    database with *something* useful and play-about-able,
    though not necessarily actual real data.
    
    One thing that'd always irked me is side effects from
    seeding the database multiple times. My first rule of
    thumb is to first empty the contents of any table you're
    seeding. Cleaner.
    
    Wait, scratch that. My actual first rule is to make absolutely
    and inviolably sure that you're not attempting to seed a
    database in a setting or an environment that could
    possibly hold valuable data. Staging/Prod. Before I run
    anything at all, check the environment, and if we're outside
    Development/Testing, taunt the user. Without remorse
    or pity.
    
    Okay, done. Now add equivalent contents of our json
    file and Promotions spec. It's okay, we'll add the file upload
    UI fiddly-bit as per the spec soon enough. This seed is
    just useful demo data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if !(Rails.env.development? || Rails.env.test?)
+  puts "Are you nuts? We're in #{Rails.env}! `rake db:seed` will wipe everything! You're a numpty. Exiting now. Actually I might do that now anyway just to teach you a lesson. Or I might not. You'll never know until you check every last little iota of your database just to make sure it's in order. Good riddance."
+end
+
+[Product, Promotion].each {|c| c.destroy_all }
+
+Product.create!([{
+  id: 1411,
+  name: 'Jockey Wheels - Orange',
+  price: 15.39,
+}, {
+  id: 33881,
+  name: 'Chain Ring 146mm',
+  price: 65.95,
+}, {
+  id: 13008,
+  name: 'Carbon Brake Pads',
+  price: 92.00,
+}, {
+  id: 9101,
+  name: 'Front Derailleur - 34.9mm',
+  price: 31.22
+}])
+
+Promotion.create!([{
+  percentage: 10,
+  threshold: 20,
+}, {
+  percentage: 15,
+  threshold: 50,
+}, {
+  percentage: 20,
+  threshold: 100,
+}])
