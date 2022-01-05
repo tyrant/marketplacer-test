0179727f47112e324ee6b9e5a540cbd480b10cde
Mikey Clarke <epicschnozz@gmail.com>
2022-01-06 12:33:13 +1300

    Implemented ShoppingCart#total and submethods
    This took quite a lot of headbutting! There is absolutely
    something to be said for well-written, testable, tested
    code. But adding actual unit test suites on top of
    everything else would shove the work hours required
    for this MVP way, way above the "just a few hours"
    you'd asked of me. This is a hello-world MVP.

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -2,56 +2,56 @@ class ShoppingCart < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
 
+  def include?(product)
+    cart_items.map(&:product).include? product
+  end
+
   # The spec is a bit vague about exactly how different promotions fight 
   # each other, so I'm going to assume that the different combos of percentages
   # and dollar-total thresholds apply progressively, like tax brackets.
   def total
-    _sum_cart_items_subtotals
-    _progressively_apply_promotions
-  end
+    sum_cart_items_subtotals
+    apply_promotional_discounts
 
-
-  #private 
-
-
-  def _sum_cart_items_subtotals
-    @pre_discount_total = cart_items.map do |cart_item|
-      cart_item.product.price * cart_item.number
-    end.sum
-  end
-
-
-  def _progressively_apply_promotions
-    @post_discount_total = 0
-
-    # To compare each Promotion's threshold value with its lower neighbour's
-    # threshold value, we can either suffer an N+1 bug or just simply yoink
-    # the entire table.
-    all_promotions = Promotion.order('threshold asc').as_json
-
-    all_promotions.each_with_index do |promotion, i|
-
-      # If our promotions values go higher than the current pre-total,
-      # calculate only up to that pre-total then exit the promotion-loop.
-      if @pre_discount_total < promotion[:threshold]
-        discountable_money = total - promotion[:threshold]
-        @post_discount_total += discountable_money * promotion[:percentage]/100
-        break
-      end
-
-      discountable_money = promotion[:threshold]
-      discountable_money -= all_promotions[i-1][:threshold] if i>0
-
-      @post_discount_total += discountable_money * promotion[:percentage]/100
-    end
-
-    # Has the total exceeded even the highest promotion threshold? Apply
-    # the maximum threshold's percentage discount to the pre-total remainder.
-    if @pre_discount_total > all_promotions.last[:threshold]
-      discountable_remainder = @pre_discount_total - all_promotions.last[:threshold]
-      @post_discount_total += discountable_remainder * all_promotions.last[:percentage]
-    end
-    
     @post_discount_total
   end
+
+
+  def sum_cart_items_subtotals
+    @pre_discount_subtotal ||= cart_items.map(&:price).sum
+  end
+
+
+  def apply_promotional_discounts
+    promotions = Promotion.where_threshold_up_to(@pre_discount_subtotal)
+
+    # We need to compare each Promotion's threshold value with its 
+    # predecessor's, so using a standard #each iterator would mean extra N+1 
+    # queries to grab those values. Easier just to convert the whole lot to an 
+    # #as_json hash and use a #with_index iterable instead
+    sum_discount_subtotals = promotions.as_json.map.with_index do |promotion, i|
+
+      discountable_for_this_threshold = if promotion[:threshold] > @pre_discount_subtotal
+          @pre_discount_subtotal - promotion[:threshold]
+        elsif i > 0
+          promotion[:threshold] - promotions[i-1][:threshold]
+        else
+          promotion[:threshold]
+        end
+
+      discountable_for_this_threshold * promotion[:percentage]/100
+
+    end.sum
+
+    # Has the subtotal exceeded even the highest threshold? Apply
+    # the maximum threshold's percentage discount to the pre-total remainder.
+    if @pre_discount_subtotal > promotions.last[:threshold]
+      discountable_remainder = @pre_discount_subtotal - promotions.last[:threshold]
+      sum_discount_subtotals += (discountable_remainder * promotions.last[:percentage]/100)
+    end
+
+    @post_discount_total ||= @pre_discount_subtotal - sum_discount_subtotals
+  end
+
+
 end
