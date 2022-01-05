e7d73d8380baf5465fec0f8b5926af8a9daf9bda
Mikey Clarke <epicschnozz@gmail.com>
2022-01-06 12:30:36 +1300

    Abstracted Promotion.where_threshold_up_to static method
    This isn't a scope! At least not a proper scope. A well-
    written scope is one you can chain it properly with other
    scopes, and retains the entire chained super-scope's
    lady loading. This doesn't. I'm not actually sure it's possible
    to query for both every Promotion with threshold < N,
    and also exactly one Promotion with threshold > N, in
    just one query, and also retain ActiveRecord's lazy-load
    doohickeys. It might be. I might be wrong. But investigating
    such a thing would be way, way beyond the scope of a
    dinky little MVP like this. Any skilfully-run business would
    shove that in their ticketing system's "Look Into Any
    Decade Now" column. Triaging, right?

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -16,4 +16,29 @@ class Promotion < ApplicationRecord
       threshold:  threshold.to_f
     }
   end
+
+
+  # We need to progressively apply to our @pre_discount_subtotal each of our
+  # stored Promotions, in ascending order of their threshold money values, up to
+  # and including the single Promotion with threshold of greater value than our 
+  # subtotal, but no further.
+  # I'd toyed with making this a scope, but I'm not sure it's possible to query
+  # both all Promotions below a threshold, plus exactly one above it, in just
+  # a single query. So it's a static method instead.
+  # Yes scopes are supposed to be syntactic sugar for static methods, but
+  # we'd expect scopes to refrain from actually executing our queries, and 
+  # instead stick to good old lazy loading. Chainable. This loads them.
+  def Promotion.where_threshold_up_to(value)
+    all_promotions_below_subtotal = Promotion
+      .where('threshold < ?', value)
+    first_promotion_above_subtotal = Promotion
+      .where('threshold > ?', value)
+      .limit(1)
+
+    [all_promotions_below_subtotal, first_promotion_above_subtotal]
+      .flatten
+      .sort_by do |promotion|
+        promotion.threshold
+      end
+  end
 end
